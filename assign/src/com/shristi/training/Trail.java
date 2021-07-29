7cdfdcd1c768a0e11744a527bbfbb15f60086b57
ayan <ayanagrawal5007@gmail.com>
2021-07-29 14:10:16 +0530

    final comit

---
+++ assign/src/com/shristi/training/Trail.java
@@ -0,0 +1,19 @@
+package com.shristi.training;
+import java.util.*;
+public class Trail {
+
+	public static void main(String[] args) {
+		//System.out.println("*****Details******\n"+"name \t"+ "AYAN");
+Scanner kb= new Scanner (System.in);
+//System.out.println("enter name");
+//String name= kb.nextLine();
+//if(name.equals("Ayan"))
+//	System.out.println("Welcome "+ name);
+//else
+//	System.out.println("name is diffrent");
+		//Scanner kb= new Scanner (System.in);
+		double salary=kb.nextDouble();
+		int number=(int) salary;
+	}
+
+}
